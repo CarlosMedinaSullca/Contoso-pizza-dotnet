56d36bbd2b5f77d5462800e4fbeb8b85e4bc2a37
CarlosMedinaSullca <carlos.medina.sullca@gmail.com>
2026-05-04 17:05:31 -0400

    Text file with evidence created

--- obj/Debug/net10.0/ContosoPizza.AssemblyInfo.cs
+++ obj/Debug/net10.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53b2abed999b18623687104669a0fc2333fb2a86")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
